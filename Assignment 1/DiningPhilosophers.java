e548e4f8474efa0b19ecefd441019e921d7c7cd2
Nahom <nahom.atnafu@mnsu.edu>
2024-02-11 23:23:43 -0600

    modified and added variables

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,16 +1,38 @@
 public class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
-    Philosopher[] philosophers = new Philosopher[5];
-    Chopstick[] chopsticks = new Chopstick[5];
+    int np = Integer.parseInt(args[0]);
+    int nc = Integer.parseInt(args[1]);
+    int tt = Integer.parseInt(args[2]);
+    int et = Integer.parseInt(args[3]);
+    int rl = Integer.parseInt(args[4]);
+
+    Philosopher[] philosophers = new Philosopher[np];
+    Chopstick[] chopsticks = new Chopstick[np+1];
     
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < np; ++i)
+    {
       chopsticks[i] = new Chopstick(i);
-    for (int i = 0; i < 5; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+    }
+
+    for (int i = 0; i < np; ++i)
+    {
+      Chopstick leftChopstick = chopsticks[i];
+      Chopstick rightChopstick = chopsticks[(i + 1) % np];
+
+      if (rl == 1 && i % 2 == 1) 
+      {
+        // odd-numbered philosophers are  left-handed
+        chopsticks[i] = rightChopstick;
+        chopsticks[(i+1) % np] = leftChopstick;
+        leftChopstick = chopsticks[i];
+        rightChopstick = chopsticks[(i+1) % np];
+      }
+
+      philosophers[i] = new Philosopher(leftChopstick, rightChopstick, tt, et, nc);
       philosophers[i].start();
     }
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < np; ++i)
       philosophers[i].join();
   }
 }
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -4,10 +4,17 @@ class Philosopher extends Thread {
   private Chopstick left, right;
   private Random random;
   private int thinkCount;
+  private int tt; //maximum thinking time in units of milliseconds
+  private int et; //is the maximum eating time, in units of milliseconds.
+  private int nc; //is the number of cycles that each philosopher will go through.
 
-  public Philosopher(Chopstick left, Chopstick right) {
+  public Philosopher(Chopstick left, Chopstick right, int tt, int et, int nc) {
     this.left = left; this.right = right;
-    random = new Random();
+    this.random = new Random();
+    this.tt = tt;
+    this.et = et;
+    this.nc = nc;
+
   }
 
   public void run() {
@@ -16,11 +23,20 @@ class Philosopher extends Thread {
         ++thinkCount;
         if (thinkCount % 10 == 0)
           System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+        Thread.sleep(random.nextInt(tt)));     // Think for a while
+        System.out.println("Philosopher " + this + " wants right chopstick");
+        synchronized(right) {       // Grab right chopstick 
+          System.out.println("Philosopher " + this + "has right chopstick");
+          System.out.println("Philosopher " + this + "wants left chopstick");
+          synchronized(left) {                    // Grab left chopstick 
+            System.out.println("Philosopher " + this + "has left chopstick");
+            System.out.println("Philosopher " + this + " eats for " + et + " units ");
+            Thread.sleep(random.nextInt(et)); // Eat for a while
+
+            // Release chopsticks
+            System.out.println("Philosopher " + this + "releases left chopstick");
           }
+          System.out.println("Philosopher " + this + "releases right chopstick");
         }
       }
     } catch(InterruptedException e) {}
