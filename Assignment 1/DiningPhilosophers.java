cc414d274045b0ac5e6de01587aaae11bdb9693e
Nahom <nahom.atnafu@mnsu.edu>
2024-02-16 23:25:32 -0600

    Created thinking method in Philosopher class Created 5 attributes that represent the philosopher's behaviours in DiningPhilosophers

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -9,34 +9,66 @@ public class DiningPhilosophers {
     // If 0 all philosophers are right-handed
     //and try to grab their right chopstick first.
     //If *rl* is 1, then even-numbered philosophers are right-handed and 
-    //odd-numbered philosophers are  left-handed.
+    //odd-numbered philosophers are left-handed.
 
     Philosopher[] philosophers = new Philosopher[np];
     Chopstick[] chopsticks = new Chopstick[np];
     
-    for (int i = 0; i < np; ++i)
+    for (int i = 0; i < np; i++)
     {
       chopsticks[i] = new Chopstick(i);
     }
 
-    for (int i = 0; i < np; ++i)
+    for (int i = 0; i < np; i++)
     {
       Chopstick leftChopstick = chopsticks[i];
       Chopstick rightChopstick = chopsticks[(i + 1) % np];
 
       if (rl == 1 && i % 2 == 1) 
       {
-        // odd-numbered philosophers are  left-handed
+        // odd-numbered philosopher
         chopsticks[i] = rightChopstick;
         chopsticks[(i+1) % np] = leftChopstick;
         leftChopstick = chopsticks[i];
         rightChopstick = chopsticks[(i+1) % np];
       }
 
-      philosophers[i] = new Philosopher(number,left, right, tt, et, nc);
+      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i+1) % np], tt, et, nc);
       philosophers[i].start();
     }
-    for (int i = 0; i < np; ++i)
-      philosophers[i].join();
-  }
+
+    while (true)
+    {
+      try
+      {
+        // sleep
+        Thread.sleep(1000);
+        
+        // check for deadlock
+        boolean deadlock = true;
+        for (Chopstick c : chopsticks)
+        {
+          if (c.isFree())
+          {
+            deadlock = false;
+            break;
+          }
+        }
+      
+          if (deadlock)
+          {
+            Thread.sleep(1000);
+            System.out.println("Everyone Eats");
+            break; // Exit loop if deadlock is detected
+          }
+        }
+      
+         
+      catch(Exception e)
+      {
+        e.printStackTrace(System.out);
+      }
+    }   
+}
+    
 }
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -4,41 +4,54 @@ import java.util.concurrent.ThreadLocalRandom;
 class Philosopher extends Thread
 {
   public int number;
-  Chopstick left= new Chopstick(1);
-  Chopstick right= new Chopstick(2);
+  Chopstick left, right;
   public int tt; //maximum thinking time in units of milliseconds
   public int et; //is the maximum eating time, in units of milliseconds.
   public int nc; //is the number of cycles that each philosopher will go through.
 
-  public Philosopher(int number, Chopstick left, Chopstick right, int tt, int et, int nc) {
-    this.number = number;
+  public Philosopher(Chopstick left, Chopstick right, int tt, int et, int nc) 
+  {  
     this.left = left; 
     this.right = right;
     this.tt = tt;
     this.et = et;
     this.nc = nc;
-
   }
 
   public void run() 
   {
     while(true)
     {
+      // Philosopher thinking
+      think();
       // We'd like the philosopher to grab the chopsticks if they're both free
       left.grab();
-      System.out.println("Philospopher " + (number + 1) + "grabs left chopstick.");
+      System.out.println("Philospopher " + (number + 1) + " grabs left chopstick.");
       right.grab();
-      System.out.println("Philospopher " + (number + 1) + "grabs right chopstick.");
+      System.out.println("Philospopher " + (number + 1) + " grabs right chopstick.");
 
       // whichever philospher is hungry starts eating
       eat();
       // after eating the philosopher releases left and right chopsticks
       left.release();
-      System.out.println("Philosopher " + (number + 1) + "releases left chopstick.");
+      System.out.println("Philosopher " + (number + 1) + " releases left chopstick.");
       right.release();
-      System.out.println("Philosopher " + (number + 1) + "releases right chopstick.");
+      System.out.println("Philosopher " + (number + 1) + " releases right chopstick.");
     }
   }
+      void think()
+      {
+        try 
+        {
+          int sleepTime = ThreadLocalRandom.current().nextInt(0, tt+1); 
+          System.out.println("Philosopher " + (number + 1) + " thinks for  "+ sleepTime + "ms");
+          Thread.sleep(sleepTime);
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace(System.out);
+        }
+      }
       void eat()
       {
         try
@@ -46,7 +59,7 @@ class Philosopher extends Thread
             // selecting a random number between 0 and 1000 representing sleep time in milliseconds
             int sleepTime = ThreadLocalRandom.current().nextInt(0, 1000);
             // Thread sleeping
-            System.out.println("Philosopher " + (number + 1) + "eats for "+ sleepTime + "ms"); 
+            System.out.println("Philosopher " + (number + 1) + " eats for "+ sleepTime + "ms"); 
             Thread.sleep(sleepTime);
         }
         catch (Exception e)
