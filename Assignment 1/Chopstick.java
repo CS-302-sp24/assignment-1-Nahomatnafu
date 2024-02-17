ca5b06b331d1eeb65cf0654f0b71d0293621b990
Nahom <nahom.atnafu@mnsu.edu>
2024-02-16 21:23:27 -0600

    Created three methods grab(), release() and isFree() using semaphores

--- Assignment 1/Chopstick.java
+++ Assignment 1/Chopstick.java
@@ -1,5 +1,37 @@
+import java.util.concurrent.Semaphore;
+ 
 class Chopstick {
 	private int id;
 	public Chopstick(int id) { this.id = id; }
  	public int getId() { return id; }
+
+	// Constructor class for Semaphore that accepts the number permits
+	public Semaphore mutex = new Semaphore(1);
+	// We create a method that allows a philosopher to grab a chopstick
+	void grab()
+	{
+		try
+		{
+			// acquiring permit from the sephamore
+			mutex.acquire();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace(System.out);
+		}
+	}
+
+	// We create a method that allows a philosopher to release a chopstick
+	void release()
+	{
+		// this releases an acquired permit and increases the number of available permits by one (chopstick)
+		mutex.release();
+	}
+
+	// Now we create a method that checks whether a chopstick is available or not
+	boolean isFree()
+	{
+		// returns true if number of available permits is greater than 0, and false otherwise
+		return mutex.availablePermits() > 0;
+	}
 }
--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,14 +1,18 @@
 public class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
-    int np = Integer.parseInt(args[0]);
-    int nc = Integer.parseInt(args[1]);
-    int tt = Integer.parseInt(args[2]);
-    int et = Integer.parseInt(args[3]);
-    int rl = Integer.parseInt(args[4]);
+    int np = Integer.parseInt(args[0]); // Number of philosophers
+    int nc = Integer.parseInt(args[1]); // Number of cycles
+    int tt = Integer.parseInt(args[2]); // Max thinking time
+    int et = Integer.parseInt(args[3]); // Max eating time
+    int rl = Integer.parseInt(args[4]); 
+    // If 0 all philosophers are right-handed
+    //and try to grab their right chopstick first.
+    //If *rl* is 1, then even-numbered philosophers are right-handed and 
+    //odd-numbered philosophers are  left-handed.
 
     Philosopher[] philosophers = new Philosopher[np];
-    Chopstick[] chopsticks = new Chopstick[np+1];
+    Chopstick[] chopsticks = new Chopstick[np];
     
     for (int i = 0; i < np; ++i)
     {
